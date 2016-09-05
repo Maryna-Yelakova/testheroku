6b4d0b0613ad330fa76ff8fe4a6a5909cf1f1635
maryna.yelakova <maryna.yelakova@gmail.com>
2016-09-05 22:22:09 +0300

    Add package.json, server.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,9 @@
-/**
- * Created by maryna.yelakova on 05.09.2016.
- */
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("btnUpdateTime").onclick = showTime;
+    showTime();
+});
+
+function showTime(){
+    var now = new Date();
+    document.getElementById("pTime").innerHTML = now;
+}
