d8037edf4e1d31beff6a75c7226cbb33e5fa4aae
maryna.yelakova <maryna.yelakova@gmail.com>
2016-09-13 18:14:07 +0300

    Change test file

--- spec/spec.js
+++ spec/spec.js
@@ -1,19 +1,12 @@
 var request = require("request");
-
+var server = require("../server.js");
 var base_url = "http://localhost:3000/";
 
-describe("Hello World Server", function() {
+describe("Server", function() {
     describe("GET /", function() {
         it("returns status code 200", function(done) {
             request.get(base_url, function(error, response, body) {
                 expect(response.statusCode).toBe(200);
-                done();
-            });
-        });
-
-        it("returns Hello World", function(done) {
-            request.get(base_url, function(error, response, body) {
-                expect(body).toBe("Hello World");
                 done();
             });
         });
