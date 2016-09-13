a14887507f7586e05817e895328788a5b770d693
maryna.yelakova <maryna.yelakova@gmail.com>
2016-09-13 22:22:38 +0300

    Change strtucture

--- spec/test.js
+++ spec/test.js
@@ -1,8 +1,8 @@
 var request = require("request");
-var server = require("../server.js");
+var testServer = require("../server.js");
 var base_url = "http://localhost:3000/";
 
-describe("Server", function() {
+describe("Hello World Server", function() {
     describe("GET /", function() {
         it("returns status code 200", function(done) {
             request.get(base_url, function(error, response, body) {
